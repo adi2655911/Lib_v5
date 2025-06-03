53bdebb5cb81a5beec533e250cd349d5681d4646
3303 ADITYA MISHRA <146878654+adi2655911@users.noreply.github.com>
2025-06-03 18:42:41 +0000

    csb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}', // Important
-    './app/**/*.{js,ts,jsx,tsx}', // Also include if `app/` exists
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
